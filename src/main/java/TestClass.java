0b2996c376bddc59d215bb79daa5c2bb887b47ee
Dhumina <witdocv5@gmail.com>
2020-12-28 02:11:32 -0500

    Basic Hello World

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,6 +1,18 @@
 
 
 public class TestClass {
-//this is a comment
-//das ist epic comment
+	//this is a comment
+	//das ist epic comment
+	
+	public static void main(String[] args) {
+		boolean initializingWorld = true;
+		int temperature = 69;
+		String weather = "Cloudy";
+		
+		if (initializingWorld && (temperature >= 60 || weather.equals("Cloudy") )) {
+			System.out.println("Hello World");
+		} else {
+			System.out.println("Goodbye World");
+		}
+	}
 }
